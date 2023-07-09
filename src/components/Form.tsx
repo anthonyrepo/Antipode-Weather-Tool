70aa7ee6f3b8aaf9b45c3519b7c6abfe532a5012
anthonyrepo <78623358+anthonyrepo@users.noreply.github.com>
2023-07-08 23:22:44 -0400

    Add states along with inline onSubmit func

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,14 @@
-function Form() {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        getAntipode();
-        e.preventDefault();
-    }
+import getAntipode from "../utils/getAntipode.tsx"
+import { useState } from "react"
 
-    const getAntipode = () => {
-        console.log("hi")
-    }
+function Form() {
+    const [latitude, setLatitude] = useState(-41.500083)
+    const [longitude, setLongitude] = useState(172.834408)
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="number" className="form-control" id="latSearch" min="-90" max="90" step="any" placeholder="Latitude" value="-41.500083" required/>
-            <input type="number" className="form-control" id="lonSearch" min="-180" max="180" step="any" placeholder="Longitude" value="172.834408" required/>
+        <form onSubmit={(e) => {getAntipode(latitude, longitude); e.preventDefault()}}>
+            <input onChange={(e) => setLatitude(Number(e.target.value))} type="number" className="form-control" id="latSearch" min="-90" max="90" step="any" placeholder="Latitude" value="-41.500083" required/>
+            <input onChange={(e) => setLongitude(Number(e.target.value))} type="number" className="form-control" id="lonSearch" min="-180" max="180" step="any" placeholder="Longitude" value="172.834408" required/>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
