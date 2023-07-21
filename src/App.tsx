5b0c84211499172f35709c45cfa4aa1d491b5a5d
anthonyrepo <78623358+anthonyrepo@users.noreply.github.com>
2023-07-20 20:51:49 -0400

    Add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Form from "./components/Form";
+import InfoBox from "./components/InfoBox"
+import { useState } from "react"
+import locationObject from "./interfaces/locationObject"
 
 function App() {
+  const [names, setNames] = useState<locationObject[]>([]);
+
+  let key = 0;
   return (
     <>
-      <Form></Form>
+      <Form callback={setNames} names={names}></Form>
+      {names.map((item) => <InfoBox key={key++} info={item}></InfoBox>)}
     </>
   )
 }
