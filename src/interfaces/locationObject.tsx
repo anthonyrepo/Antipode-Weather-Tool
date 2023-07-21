9385f8c91f02637fe91f4352ab1c8744becf1a37
anthonyrepo <78623358+anthonyrepo@users.noreply.github.com>
2023-07-20 20:54:43 -0400

    Add locationObject interface

---
+++ src/interfaces/locationObject.tsx
@@ -0,0 +1,10 @@
+interface locationObject {
+    name: string;
+    description: string;
+    feels_like: number;
+    humidity: number;
+    temperature: number;
+    weather: string;
+}
+
+export default locationObject;
