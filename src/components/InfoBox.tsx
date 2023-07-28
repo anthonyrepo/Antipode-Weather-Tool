35d6fe1a04b930aa63601b538f12bb7f7fe7fc60
anthonyrepo <78623358+anthonyrepo@users.noreply.github.com>
2023-07-28 17:34:22 -0400

    Add full functionality

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,13 +1,23 @@
 import locationObject from "../interfaces/locationObject";
+import toFahrenheit from "../utils/toFahrenheit";
+import toCelsius from "../utils/toCelsius";
+
+import "../stylesheets/InfoBox.css"
 
 interface Props {
     info: locationObject;
 }
 
 function InfoBox({ info }: Props) {
+    const description = info.description.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+
     return (
-        <div>
-            <h1>{info.name}</h1>
+        <div id="card" className="col-md-6">
+            <h3>{info.name}</h3>
+            <h3>{description}</h3>
+            <h3>{`Temperature: ${toFahrenheit(info.temperature)}°F (${toCelsius(info.temperature)}°C)`}</h3>
+            <h3>{`Feels Like: ${toFahrenheit(info.feels_like)}°F (${toCelsius(info.feels_like)}°C)`}</h3>
+            <h3>{`Humidity: ${info.humidity}%`}</h3>
         </div>
     );
 }
