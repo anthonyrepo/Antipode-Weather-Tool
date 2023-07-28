9b5e30aa486ee8b160b3326b20e48ad941ed214b
anthonyrepo <78623358+anthonyrepo@users.noreply.github.com>
2023-07-28 17:43:56 -0400

    Add stylesheet, add subtitles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import InfoBox from "./components/InfoBox"
 import { useState } from "react"
 import locationObject from "./interfaces/locationObject"
 
+import "./stylesheets/App.css"
+
 function App() {
   const [names, setNames] = useState<locationObject[]>([]);
 
   let key = 0;
   return (
     <>
+      <h1 id="title">Antipode Weather Tool</h1>
       <Form callback={setNames} names={names}></Form>
-      {names.map((item) => <InfoBox key={key++} info={item}></InfoBox>)}
+      <div className="flex">
+        {Boolean(names.length) && <h3 id="card-subtitle">Provided Coordinates</h3>}
+        {Boolean(names.length) && <h3 id="card-subtitle">Antipode</h3>}
+        {names.map((item) => <InfoBox key={key++} info={item}></InfoBox>)}
+      </div>
     </>
   )
 }
